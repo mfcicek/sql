f79d0c76a6127a6e32f82f07657e7ebe51302f5f
Fatih Çiçek <44978096+mfcicek@users.noreply.github.com>
2021-11-09 15:11:57 +0300

    Update odev-6.sql

--- odev-6.sql
+++ odev-6.sql
@@ -15,4 +15,4 @@
 4) film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
 
     SELECT COUNT(DISTINCT replacement_cost) FROM film
-    WHERE length > 150
+    WHERE length > 150;
